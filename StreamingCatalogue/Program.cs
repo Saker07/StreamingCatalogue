1758c27770abe3897eed3543198d5075e7f07a67
Klaudio Gjoni <gjoni.klaudio@gmail.com>
2025-02-15 23:33:32 +0000

    Add sample

--- StreamingCatalogue/Program.cs
+++ StreamingCatalogue/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks.Dataflow;
+using StreamingCatalogue;
+
+StreamingRegister test = new StreamingRegister();
+
+Film film = new Film("film1", new DateOnly(), "horror", 60);
+
+test.AddStreamingService(new StreamingService("Notflix", 12.99M));
+test.AddFilm(film, "Notflix");
+
+ReadOnlyCollection<(string StreamingServiceName, IFilm Film)>? noftlixFilms = test.GetAllFilmsInStreamingService("Notflix");
+
+if (noftlixFilms != null)
+{
+    foreach (var x in noftlixFilms)
+    {
+        Console.WriteLine($"{x.Film.Name}, {x.Film.Genre}, {x.Film.ReleaseDate}, {x.Film.Rating}");
+    }
+}
